b7dec04fa07ff389c45c4ec48dcc094e26297972
ezekiel <ezekielx.nju@vip.163.com>
2025-06-04 23:58:58 +0800

    finish chapter 16

--- tinyalu_bfm.sv
+++ tinyalu_bfm.sv
@@ -1,20 +1,23 @@
 /*
-   Copyright 2013 Ray Salemi
+ Copyright 2013 Ray Salemi
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
 
-       http://www.apache.org/licenses/LICENSE-2.0
+ http://www.apache.org/licenses/LICENSE-2.0
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 interface tinyalu_bfm;
    import tinyalu_pkg::*;
+
+   command_monitor command_monitor_h;
+   result_monitor  result_monitor_h;
 
    byte         unsigned        A;
    byte         unsigned        B;
@@ -26,16 +29,10 @@ interface tinyalu_bfm;
    wire [15:0]  result;
    operation_t  op_set;
 
+
+
+
    assign op = op_set;
-
-   initial begin
-      clk = 0;
-      forever begin
-         #10;
-         clk = ~clk;
-      end
-   end
-
 
    task reset_alu();
       reset_n = 1'b0;
@@ -44,12 +41,9 @@ interface tinyalu_bfm;
       reset_n = 1'b1;
       start = 1'b0;
    endtask : reset_alu
-   
-  task send_op(input byte iA, input byte iB, input operation_t iop, output shortint alu_result);
-     
+
+   task send_op(input byte iA, input byte iB, input operation_t iop, shortint result);
       if (iop == rst_op) begin
-         @(negedge clk);
-         op_set = iop;         
          @(posedge clk);
          reset_n = 1'b0;
          start = 1'b0;
@@ -65,18 +59,48 @@ interface tinyalu_bfm;
          if (iop == no_op) begin
             @(posedge clk);
             #1;
-            start = 1'b0;           
+            start = 1'b0;
          end else begin
             do
-              @(negedge clk);
+               @(negedge clk);
             while (done == 0);
             start = 1'b0;
          end
-         alu_result = result;
       end // else: !if(iop == rst_op)
-      
+
    endtask : send_op
 
-endinterface : tinyalu_bfm
 
-   
+   always @(posedge clk) begin : cmd_monitor
+      bit new_command;
+      if (!start)
+         new_command = 1;
+      else
+         if (new_command) begin
+            command_monitor_h.write_to_monitor(A, B, op);
+            new_command = (op == 3'b000); // handle no_op
+         end
+   end : cmd_monitor
+
+   always @(negedge reset_n) begin : rst_monitor
+      if (command_monitor_h != null) //guard against VCS time 0 negedge
+         command_monitor_h.write_to_monitor(A, B, rst_op);
+   end : rst_monitor
+
+
+   always @(posedge clk) begin : rslt_monitor
+      if (done)
+         result_monitor_h.write_to_monitor(result);
+   end : rslt_monitor
+
+
+   initial begin
+      clk = 0;
+      forever begin
+         #10;
+         clk = ~clk;
+      end
+   end
+
+
+endinterface : tinyalu_bfm
